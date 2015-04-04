587e52d3bed8dc0f6ff05013b3201c80ead4540b
Scottie Enriquez <scott.c.enriquez@gmail.com>
2015-04-04 18:42:06 -0500

    Adding setup folder

---
+++ setup/setupRethink.js
@@ -0,0 +1,24 @@
+//Node.js version
+var rethinkdb = require('rethinkdb');
+
+//@to-do: update for dynamic host information
+var connection = null;
+//establish connection
+//callback: create bluetooth_node database
+rethinkdb.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+    if (err) throw err;
+    connection = conn;
+    //create bluetooth_node database
+    //calback: create tables
+    rethinkdb.dbCreate('bluetooth_node').run(connection, function(err, result) {
+        if (err) throw err;
+        rethinkdb.db('bluetooth_node').tableCreate('bluetooth_devices').run(connection, function(err, result) {
+            if (err) throw err;
+            console.log(JSON.stringify(result, null, 2));
+        });
+        rethinkdb.db('bluetooth_node').tableCreate('bluetooth_connections').run(connection, function(err, result) {
+            if (err) throw err;
+            console.log(JSON.stringify(result, null, 2));
+        });
+    });
+});
