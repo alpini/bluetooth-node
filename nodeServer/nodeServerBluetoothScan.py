9dc1516a648628726838f6edf58c9e681517e9f8
Scottie Enriquez <scott.c.enriquez@gmail.com>
2015-04-04 21:40:32 -0500

    First connection

--- nodeServer/nodeServerBluetoothScan.py
+++ nodeServer/nodeServerBluetoothScan.py
@@ -4,6 +4,6 @@ from rethinkdb.errors import RqlRuntimeError, RqlDriverError
 
 #open connection to RethinkDB running on MacBook Pro
 connection = rethinkdb.connect(host='localhost', port=28015)
-devices = rethinkdb.table('bluetooth_devices').run()
+devices = rethinkdb.db('bluetooth_node').table('bluetooth_devices').run(connection)
 for device in devices:
     print device
