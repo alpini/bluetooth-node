f3d05c08efbbee49edc762e84e8040d7a6a07860
Scottie Enriquez <scott.c.enriquez@gmail.com>
2015-04-05 15:34:13 -0500

    Added process forks to SSH commands in the shell script

--- rethinkSetup/remote/remoteConnect.sh
+++ rethinkSetup/remote/remoteConnect.sh
@@ -6,12 +6,12 @@ echo "Home IP addresses configured..."
 
 ssh -N compose@aws-us-east-1-portal.3.dblayer.com -p 10056 \
 -L 127.0.0.2:28015:10.20.128.2:28015 \
--L 127.0.0.3:28015:10.20.128.3:28015
+-L 127.0.0.3:28015:10.20.128.3:28015 &
 
 echo "Driver SSH tunnel configured..."
 
 ssh -N compose@aws-us-east-1-portal.3.dblayer.com -p 10056 \
 -L 127.0.0.2:8080:10.20.128.2:8080 \
--L 127.0.0.3:8080:10.20.128.3:8080 
+-L 127.0.0.3:8080:10.20.128.3:8080 &
 
 echo "Admin UI SSH tunnel configured..."
