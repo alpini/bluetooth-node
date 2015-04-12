748bf40431bb8ddcef930b3a3ac90db767ca372d
Scottie Enriquez <scott.c.enriquez@gmail.com>
2015-04-12 13:30:11 -0500

    Progress on JavaScript node server code

--- node/bluetoothNodeServer.js
+++ node/bluetoothNodeServer.js
@@ -1,19 +1,41 @@
 //Node.js version
 var rethinkdb = require('rethinkdb');
 
-var connection = null;
-rethinkdb.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+//open connection to database
+//callback pulls a list of devices
+var databaseConnection = null;
+rethinkdb.connect({host: '127.0.0.2', port: 28015}, function(err, conn) {
     if (err) throw err;
-    connection = conn;
-    rethinkdb.dbCreate('bluetooth_node').run(connection, function(err, result) {
+    databaseConnection = conn;
+    rethinkdb.db('bluetooth_node').table('bluetooth_devices').run(databaseConnection, function(err, cursor) {
         if (err) throw err;
-        rethinkdb.db('bluetooth_node').tableCreate('bluetooth_devices').run(connection, function(err, result) {
-            if (err) throw err;
-            console.log(JSON.stringify(result, null, 2));
-        });
-        rethinkdb.db('bluetooth_node').tableCreate('bluetooth_connections').run(connection, function(err, result) {
-            if (err) throw err;
-            console.log(JSON.stringify(result, null, 2));
+        cursor.toArray(function(err, result) {
+            if(err) throw err;
+            var devices = result;
+            //run indefinitely
+            while (true) {
+                //check changefeed
+                rethinkdb.db('bluetooth_node').table('bluetooth_devices').changes().run(databaseConnection, function(err, cursor) {
+                    console.log('here');
+                    changeFeed = cursor;
+                    changesFound = false;
+                    for (var change in changeFeed) {
+                        changesFound = true;
+                    }
+                    if (changesFound) {
+                        rethinkdb.db('bluetooth_node').table('bluetooth_devices').run(databaseConnection, function(err, cursor) {
+                            if (err) throw err;
+                            cursor.toArray(function(err, result) {
+                                if(err) throw err;
+                                var devices = result;
+                            });
+                        });
+                    }
+                    for (var device in devices) {
+
+                    }
+                });
+            }
         });
     });
 });
