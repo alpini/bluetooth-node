22ae350d4f430fa5399ed222c430d8a2b9a81934
Scottie Enriquez <scott.c.enriquez@gmail.com>
2015-04-11 23:57:59 -0500

    Changefeeds implemented for node server

--- nodeServer/bluetoothNodeServer.py
+++ nodeServer/bluetoothNodeServer.py
@@ -7,12 +7,21 @@ import rethinkdb as rethinkdb
 from rethinkdb.errors import RqlRuntimeError, RqlDriverError
 
 databaseConnection = rethinkdb.connect(host='127.0.0.2', port=28015)
+devices = rethinkdb.db('bluetooth_node').table('bluetooth_devices').run(databaseConnection)
 #run indefinitely
 while (True):
-	#pull fresh list of devices
-	devices = rethinkdb.db('bluetooth_node').table('bluetooth_devices').run(databaseConnection)
+    #pull fresh list of devices
+    changeFeed = rethinkdb.db('bluetooth_node').table('bluetooth_devices').changes().run(databaseConnection)
+    #check for changes in the changefeed
+    changesFound = False
+    for change in changeFeed:
+        changesFound = True
+        break
+    #if there are changes, do a fresh pull of devices
+    if (changesFound):
+        devices = rethinkdb.db('bluetooth_node').table('bluetooth_devices').run(databaseConnection)
     for device in devices:
-    	#poll for device on node server
+        #poll for device on node server
         output = subprocess.check_output(['hcitool', 'name', device['device_address']])
         #if present, write timestamp to database
         if (output != ''):
