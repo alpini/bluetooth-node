fd91d98709469c54969812db0a212dc18696c0f7
Scottie Enriquez <scott.c.enriquez@gmail.com>
2015-04-11 23:20:44 -0500

    Initial node server functionality in Python

--- nodeServer/bluetoothNode.py
+++ nodeServer/bluetoothNode.py
@@ -1,11 +1,23 @@
 #Python version
 import os
 import subprocess
+import time
+import datetime
 import rethinkdb as rethinkdb
 from rethinkdb.errors import RqlRuntimeError, RqlDriverError
 
 databaseConnection = rethinkdb.connect(host='127.0.0.2', port=28015)
-devices = rethinkdb.db('bluetooth_node').table('bluetooth_devices').run(databaseConnection)
-for device in devices:
-    response = subprocess.call(["hcitool", "name", device.device_address])
-    print response
+#run indefinitely
+while (True):
+	#pull fresh list of devices
+	devices = rethinkdb.db('bluetooth_node').table('bluetooth_devices').run(databaseConnection)
+    for device in devices:
+    	#poll for device on node server
+        output = subprocess.check_output(['hcitool', 'name', device['device_address']])
+        #if present, write timestamp to database
+        if (output != ''):
+            phoneName = output.split('\n')[0]
+            exactTime = time.time()
+            timeStamp = datetime.datetime.fromtimestamp(exactTime).strftime('%Y-%m-%d %H:%M:%S')
+            rethinkdb.db('bluetooth_node').table('bluetooth_connections').insert({'bluetooth_address' : device['device_address'], 'phone_name' : phoneName, 'time_stamp' : timeStamp}).run(databaseConnection)
+    time.sleep(1)
